78176ba0bbe7583834f6042f69f17b7b998ad3eb
Barbod Masoudi / <barbodmasoudi@gmail.com>
2024-12-29 12:28:15 +0330

    update (write comments)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import ImageUploader from "./UploadFile";
 
 export default function App() {
   const [imagesName, setImagesName] = useState();
+
+  console.log(imagesName); // now you have the name of images
   
   return (
     <>
--- src/UploadFile/index.jsx
+++ src/UploadFile/index.jsx
@@ -1,191 +1,3 @@
-// // Import necessary modules
-// import CloudUploadOutlinedIcon from "@mui/icons-material/CloudUploadOutlined";
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import {
-//   Box,
-//   Button,
-//   CircularProgress,
-//   IconButton,
-//   Paper,
-//   Stack,
-// } from "@mui/material";
-// import React, { useState } from "react";
-
-// const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ7XCJ1c2VyX2NvZGVcIjogXCIxMjM0NVwifSIsImV4cCI6MTczNzk4MjkxMy4zOTc5NDZ9.EC6xy18jc_KuSJL1q_CMIq_l4zLWcJYOvQWprCtu5pI`;
-
-// const ImageUploader = ({ setImagesName }) => {
-//   const [images, setImages] = useState([]);
-//   const [isDragging, setIsDragging] = useState(false);
-//   const [isUploading, setIsUploading] = useState(false);
-
-//   const processFiles = (files) => {
-//     const validFiles = Array.from(files).filter((file) =>
-//       file.type.startsWith("image/")
-//     );
-//     validFiles.forEach((file) => {
-//       const reader = new FileReader();
-//       reader.onload = () =>
-//         setImages((prev) => [...prev, { file, preview: reader.result }]);
-//       reader.readAsDataURL(file);
-//     });
-//   };
-
-//   const handleDrop = (event) => {
-//     event.preventDefault();
-//     setIsDragging(false);
-//     processFiles(event.dataTransfer.files);
-//   };
-
-//   const handleFileChange = (event) => {
-//     processFiles(event.target.files);
-//   };
-
-//   const clearImage = (index) => {
-//     setImages((prev) => prev.filter((_, i) => i !== index));
-//   };
-
-//   // the function who write the image to the server
-
-//   const uploadImages = async () => {
-//     setIsUploading(true);
-//     try {
-//       const formData = new FormData();
-//       images.forEach(({ file }) => formData.append("file", file));
-//       const response = await fetch(
-//         "https://api.mealland.ir/v1/pa/files/image",
-//         {
-//           method: "POST",
-//           headers: {
-//             Authorization: `Bearer ${token}`,
-//           },
-//           body: formData,
-//         }
-//       );
-//       const data = await response.json();
-//       setImagesName(data?.data?.file_name);
-//       // alert("Images uploaded successfully!");
-//     } catch (error) {
-//       console.error("Error uploading images:", error);
-//       // alert("Failed to upload images.");
-//     } finally {
-//       setIsUploading(false);
-//     }
-//   };
-
-//   // image preview items
-//   const items = images?.map((image, index) => (
-//     <Paper
-//       key={index}
-//       elevation={3}
-//       sx={{
-//         padding: '5px 30px',
-//         textAlign: "center",
-//         // width: "150px",
-//         position: "relative",
-//       }}
-//     >
-//       <img
-//         src={image?.preview}
-//         alt={`Preview ${index + 1}`}
-//         style={{
-//           maxWidth: "100%",
-//           maxHeight: "100px",
-//           borderRadius: "8px",
-//         }}
-//       />
-//       <IconButton
-//         color="error"
-//         onClick={() => clearImage(index)}
-//         sx={{ position: "absolute", top: 0, right: 0,p:0 }}
-//       >
-//         <DeleteIcon />
-//       </IconButton>
-//     </Paper>
-//   ));
-
-//   return (
-//     <Stack alignItems={"center"} gap="10px" sx={{ my: "100px" }}>
-//       {/* the drop paper */}
-//       <Paper
-//         component={Stack}
-//         alignItems={"center"}
-//         justifyContent={"center"}
-//         elevation={3}
-//         sx={{
-//           border: "2px dashed",
-//           borderColor: isDragging ? "primary.main" : "grey.400",
-//           borderRadius: 2,
-//           textAlign: "center",
-//           transition: "border-color 0.3s, box-shadow 0.3s",
-//           boxShadow: isDragging ? "0 0 10px rgba(0, 0, 0, 0.2)" : "none",
-//           cursor: "pointer",
-//           p: "10px",
-//           width: 200,
-//           height: 100,
-//         }}
-//         onClick={() => document.getElementById("fileInput").click()}
-//         onDragOver={(e) => {
-//           e.preventDefault();
-//           setIsDragging(true);
-//         }}
-//         onDragLeave={() => setIsDragging(false)}
-//         onDrop={handleDrop}
-//       >
-//         <IconButton>
-//           <CloudUploadOutlinedIcon
-//             color={isDragging ? "primary" : "textSecondary"}
-//           />
-//         </IconButton>
-//         <input
-//           id="fileInput"
-//           type="file"
-//           accept="image/*"
-//           multiple
-//           hidden
-//           onChange={handleFileChange}
-//         />
-//       </Paper>
-//       {/* choose  file */}
-//       <Button
-//         variant="outlined"
-//         onClick={() => document.getElementById("fileInput").click()}
-//         sx={{ bgcolor: "black", color: "white", fontWeight: "bolder" }}
-//       >
-//         انتخاب فایل
-//       </Button>
-//       {/* preview box */}
-//       <Box
-//         sx={{
-//           display: "flex",
-//           flexWrap: "wrap",
-//           gap: 2,
-//           justifyContent: "center",
-//         }}
-//       >
-//         {items}
-//       </Box>
-//       {/* if user choose file than show upload button */}
-//       {images.length > 0 && (
-//         <Button
-//           variant="outlined"
-//           onClick={uploadImages}
-//           disabled={isUploading}
-//           sx={{
-//             bgcolor: isUploading ? "grey.400" : "black",
-//             color: "white",
-//             fontWeight: "bolder",
-//             width: "100px",
-//           }}
-//         >
-//           {isUploading ? <CircularProgress size="25px" /> : "آپلود"}
-//         </Button>
-//       )}
-//     </Stack>
-//   );
-// };
-
-// export default ImageUploader;
-
 // Import necessary modules
 import React, { useState, useCallback, useMemo } from "react";
 import CloudUploadOutlinedIcon from "@mui/icons-material/CloudUploadOutlined";
@@ -199,17 +11,19 @@ import {
   CircularProgress,
 } from "@mui/material";
 
-const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ7XCJ1c2VyX2NvZGVcIjogXCIxMjM0NVwifSIsImV4cCI6MTczNzk4MjkxMy4zOTc5NDZ9.EC6xy18jc_KuSJL1q_CMIq_l4zLWcJYOvQWprCtu5pI`;
-
 const ImageUploader = ({ setImagesName }) => {
-  const [images, setImages] = useState([]);
-  const [isDragging, setIsDragging] = useState(false);
-  const [isUploading, setIsUploading] = useState(false);
+  const [images, setImages] = useState([]); //state to set images file
+  const [isDragging, setIsDragging] = useState(false); //state to check if file is being dragged
+  const [isUploading, setIsUploading] = useState(false); //state to check if file is being uploaded
 
+  // this function read all the files selected by the user
+  // and if all the files format is be true its add to images state
   const processFiles = useCallback((files) => {
+    // if file be valid or not
     const validFiles = Array.from(files).filter((file) =>
       file.type.startsWith("image/")
     );
+    // add each file into images state
     validFiles.forEach((file) => {
       const reader = new FileReader();
       reader.onload = () => {
@@ -219,6 +33,8 @@ const ImageUploader = ({ setImagesName }) => {
     });
   }, []);
 
+  // this function read all the files draped by the user
+  // and it's atomically call the processFiles function
   const handleDrop = useCallback(
     (event) => {
       event.preventDefault();
@@ -230,6 +46,7 @@ const ImageUploader = ({ setImagesName }) => {
     [processFiles]
   );
 
+  // this function read all the files selected by the user
   const handleFileChange = useCallback(
     (event) => {
       if (event.target && event.target.files) {
@@ -239,25 +56,28 @@ const ImageUploader = ({ setImagesName }) => {
     [processFiles]
   );
 
+  // this function will be clear the file from images state
   const clearImage = useCallback((index) => {
     setImages((prev) => prev.filter((_, i) => i !== index));
   }, []);
 
+  // here where you can write the request and get the filename from the server
   const uploadImages = useCallback(async () => {
     setIsUploading(true);
     try {
       const formData = new FormData();
       images.forEach(({ file }) => formData.append("file", file));
 
-      const response = await fetch("https://api.mealland.ir/v1/pa/files/image", {
+      const response = await fetch(import.meta.env.VITE_API, {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
         },
         body: formData,
       });
 
       const data = await response.json();
+      // i usually send the filename to the parent state
       setImagesName(data?.data?.file_name);
       // alert("Images uploaded successfully!");
     } catch (error) {
@@ -268,6 +88,7 @@ const ImageUploader = ({ setImagesName }) => {
     }
   }, [images, setImagesName]);
 
+  // preview items
   const items = useMemo(
     () =>
       images.map((image, index) => (
@@ -281,6 +102,7 @@ const ImageUploader = ({ setImagesName }) => {
             position: "relative",
           }}
         >
+          {/* image preview */}
           <img
             src={image.preview}
             alt={`Preview ${index + 1}`}
@@ -290,6 +112,7 @@ const ImageUploader = ({ setImagesName }) => {
               borderRadius: "8px",
             }}
           />
+          {/* icon to remove image from images state */}
           <IconButton
             color="error"
             onClick={() => clearImage(index)}
@@ -303,7 +126,7 @@ const ImageUploader = ({ setImagesName }) => {
   );
 
   return (
-    <Stack alignItems={"center"} gap="10px" sx={{ my: "100px" }}>
+    <Stack alignItems={"center"} gap="10px" sx={{ my: "100px", }}>
       {/* Drag-and-Drop Area */}
       <Paper
         component={Stack}
@@ -331,9 +154,9 @@ const ImageUploader = ({ setImagesName }) => {
         onDrop={handleDrop}
       >
         {/* <IconButton> */}
-          <CloudUploadOutlinedIcon
-            color={isDragging ? "primary" : "textSecondary"}
-          />
+        <CloudUploadOutlinedIcon
+          color={isDragging ? "primary" : "textSecondary"}
+        />
         {/* </IconButton> */}
         <input
           id="fileInput"
@@ -376,7 +199,11 @@ const ImageUploader = ({ setImagesName }) => {
             width: "100px",
           }}
         >
-          {isUploading ? <CircularProgress size={20} sx={{ color: "white" }} /> : "آپلود"}
+          {isUploading ? (
+            <CircularProgress size={20} sx={{ color: "white" }} />
+          ) : (
+            "آپلود"
+          )}
         </Button>
       )}
     </Stack>
